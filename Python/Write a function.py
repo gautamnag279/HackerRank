abf6fdb9dd763c553f59b7f34f92ab9429200ae4
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2020-12-11 19:29:53 +0530

    Update Write a function.py

--- Python/Write a function.py
+++ Python/Write a function.py
@@ -1,15 +1,11 @@
 def is_leap(year):
-    if (year % 4) == 0: 
-        if (year % 100) == 0: 
-            if (year % 400) == 0: 
-                return True
-            else: 
-                return False
-        else: 
-             return True
+    if (year % 4) == 0 and (year % 100) == 0: and (year % 400) == 0:
+        return True
     else: 
         return False
-    
-    # Write your logic here
+    else: 
+         return True
+    else: 
+        return False
     
     return leap

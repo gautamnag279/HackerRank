0848006ce776056d3d2d1577fd2f212f3e148bb1
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2020-12-11 19:25:28 +0530

    Update If - Else.py

--- Python/If - Else.py
+++ Python/If - Else.py
@@ -1,8 +1,7 @@
 num = int(input(""))
 if num % 2 != 0:
     print("Weird")
-if num % 2 == 0:
-    if 6 <= num <= 20:
+if num % 2 == 0 and 6 <= num <= 20:
         print("Weird")
     else:
         print("Not Weird")

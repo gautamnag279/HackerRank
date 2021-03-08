4e432da8467f3b65272ddbfaf489ef46bc7f9e2f
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-03-08 10:06:27 +0530

    Update Migratory Birds.py

--- Problem Solving/Migratory Birds.py
+++ Problem Solving/Migratory Birds.py
@@ -1,25 +1,19 @@
-#Using the count sort algorithm
-num = input()
-count = [0]*6
-for i in map(int , input().strip().split()):
-    count[i] = count[i] + 1
-print(count.index(max(count)))
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
-#Using HashMap
+# Complete the migratoryBirds function below.
 def migratoryBirds(arr):
-    most = 1
-    hashMap = {1:0,2:0,3:0,4:0,5:0}
-    for i in arr:
-        hashMap[i]+=1  
-        if hashMap[i] > hashMap[most]:
-            most = i
-        elif hashMap[i] == hashMap[most] :
-            if i < most:
-                most = i
-            
-    return most
-print(migratoryBirds([2, 2, 1, 1]))
+    Max , Element=-1,-1
+    for i in range(1,6):
+        if Max < arr.count(i):
+            Max = arr.count(i)
+            Element=i
+    return Element
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
@@ -32,4 +26,5 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
 

8924baef28951e7767e689c2c93e8fc9c13f4aaa
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-05-06 20:37:27 +0530

    Update Mutations.py

--- Python/Mutations.py
+++ Python/Mutations.py
@@ -9,3 +9,18 @@ if __name__ == '__main__':
     i, c = input().split()
     s_new = mutate_string(s, int(i), c)
     print(s_new)
+
+    
+    
+    
+    
+# I JUST FOUND A WAY EASIER WAY TO TO THIS ON STACK OVER.
+def mutate_string(string, position, character):
+    s = string[:position] + character + string[position+1:]
+    return s
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)

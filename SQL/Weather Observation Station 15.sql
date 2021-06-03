217b4692aaa3823e8240e2b56b89b2baf24ca382
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-06-03 21:19:34 +0530

    Update Weather Observation Station 15.sql

--- SQL/Weather Observation Station 15.sql
+++ SQL/Weather Observation Station 15.sql
@@ -2,5 +2,3 @@ SELECT ROUND(LONG_W, 4) FROM STATION
 WHERE LAT_N < 137.2345
 ORDER BY LAT_N DESC 
 LIMIT 1;
-
-#The MAX() must be followed by either GROUP/ORDER/...etc(ORDER BY in this case)for the query to work.

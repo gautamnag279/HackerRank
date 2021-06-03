22f7370896f73d3985e7c469ab66e60377526f38
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-06-03 21:15:59 +0530

    Create Weather Observation Station 15.sql

---
+++ SQL/Weather Observation Station 15.sql
@@ -0,0 +1,6 @@
+SELECT ROUND(LONG_W, 4) FROM STATION
+WHERE MAX(LAT_N) < 137.2345
+ORDER BY LAT_N DESC 
+LIMIT 1;
+
+#The MAX() must be followed by either GROUP/ORDER/...etc(ORDER BY in this case)for the query to work.

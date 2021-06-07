c8b5f20af1f3bdc439224a16bad2d892361c8aa6
Gautam Nag <51375618+gautamnag279@users.noreply.github.com>
2021-06-07 20:46:31 +0530

    Update Library Fine.py

--- Python/Library Fine.py
+++ Python/Library Fine.py
@@ -8,7 +8,7 @@ import sys
 
 def libraryFine(d1, m1, y1, d2, m2, y2):
     if y1 > y2:
-        return 1000
+        return 10000
     elif y1==y2 and m1>m2:
         return (m1-m2)*500
     elif y1==y2 and m1==m2 and d1>d2:
